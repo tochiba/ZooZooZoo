faee976b51848870ca03b1fc1c910643326373f0
tochiba <c104kn@gmail.com>
2016-03-26 16:20:39 +0900

    first commit

---
+++ ZooZooZoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZooZooZoo
+//
+//  Created by 千葉 俊輝 on 2016/03/26.
+//  Copyright © 2016年 Toshiki Chiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
