4e12ad7c4866ac6b7b4d7085b140a5bfe080d192
tochiba <c104kn@gmail.com>
2016-04-02 00:10:43 +0900

    Add Video model

---
+++ ZooZooZoo/Model/AnimalMovie.swift
@@ -0,0 +1,23 @@
+//
+//  AnimalMovie.swift
+//  ZooZooZoo
+//
+//  Created by tochiba on 2016/03/30.
+//  Copyright © 2016年 Toshiki Chiba. All rights reserved.
+//
+
+import Foundation
+
+class AnimalVideo {
+    var id: String              = ""
+    var date: String            = ""
+    var title: String           = ""
+    var descriptioin: String    = ""
+    var thumbnailUrl: String    = ""
+    var videoUrl: String        = ""
+}
+
+class Like {
+    var id: String              = ""
+    var likeCount: Int          = 0
+}
